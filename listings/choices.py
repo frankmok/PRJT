cdebc700501c566419b88ec0a09fc29d253cebd5
frankmok <frankmokwaitak@gmail.com>
2024-09-27 02:17:03 +0800

    Step 12: some minor changes on search options

--- listings/choices.py
+++ listings/choices.py
@@ -8,11 +8,11 @@ colors_set = {listing.color for listing in listings}
 color_choices = {'Reset Color (Any)': ''} | {color: color for color in colors_set}
 
 max_price = max({listing.price for listing in listings})
-
 grade = 200000
-
 budget_choices = {'Reset Budget (Any)': '0'} | {'HK$'+f'{g:,}': str(g) for g in range(grade, max_price + grade, grade)}
 
-hand_drive_choices = {'Reset Hand Drive (Any)': ''} | {listing.hand_drive + '-hand-drive': listing.hand_drive for listing in listings}
+hand_drives_set = {listing.hand_drive for listing in listings}
+hand_drive_choices = {'Reset Hand Drive (Any)': ''} | {hand_drive + '-hand-drive': hand_drive for hand_drive in hand_drives_set}
 
-wheels_drive_choices = {'Reset Wheels Drive (Any)': ''} | {listing.wheels_drive + '-wheels-drive': listing.wheels_drive for listing in listings}
+wheels_drives_set = {listing.wheels_drive for listing in listings}
+wheels_drive_choices = {'Reset Wheels Drive (Any)': ''} | {wheels_drive + '-wheels-drive': wheels_drive for wheels_drive in wheels_drives_set}
