da8e3489f4c8171e1d05e5821fdcdaee1d8af9c9
frankmok <frankmokwaitak@gmail.com>
2024-09-27 10:20:46 +0800

    Step 13: minor changes done, before ppt

--- listings/choices.py
+++ listings/choices.py
@@ -2,17 +2,17 @@ from listings.models import Listing
 
 listings = Listing.objects.all().filter(is_published=True)
 
-brand_model_choices = {'Reset Both Brand and Model (Any)': ''} | {listing.brand + '  ' + listing.model: listing.brand + '~' + listing.model for listing in listings}
+brand_model_choices = {'Reset Both Brand and Model to a null criterion': ''} | {listing.brand + '  ' + listing.model: listing.brand + '~' + listing.model for listing in listings}
 
 colors_set = {listing.color for listing in listings}
-color_choices = {'Reset Color (Any)': ''} | {color: color for color in colors_set}
+color_choices = {'Reset Color to a null criterion': ''} | {color: color for color in colors_set}
 
 max_price = max({listing.price for listing in listings})
 grade = 200000
-budget_choices = {'Reset Budget (Any)': '0'} | {'HK$'+f'{g:,}': str(g) for g in range(grade, max_price + grade, grade)}
+budget_choices = {'Reset Budget to a null criterion': '0'} | {'HK$'+f'{g:,}': str(g) for g in range(grade, max_price + grade, grade)}
 
 hand_drives_set = {listing.hand_drive for listing in listings}
-hand_drive_choices = {'Reset Hand Drive (Any)': ''} | {hand_drive + '-hand-drive': hand_drive for hand_drive in hand_drives_set}
+hand_drive_choices = {'Reset Hand Drive to a null criterion': ''} | {hand_drive + '-hand-drive': hand_drive for hand_drive in hand_drives_set}
 
 wheels_drives_set = {listing.wheels_drive for listing in listings}
-wheels_drive_choices = {'Reset Wheels Drive (Any)': ''} | {wheels_drive + '-wheels-drive': wheels_drive for wheels_drive in wheels_drives_set}
+wheels_drive_choices = {'Reset Wheels Drive to a null criterion': ''} | {wheels_drive + '-wheels-drive': wheels_drive for wheels_drive in wheels_drives_set}
